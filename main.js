e8d04b8d593fc4b86f35f71335beeef985853249
ReenaKhan143 <alireena217@gmail.com>
2024-06-05 01:39:00 -0700

    push my mini oop project

---
+++ main.js
@@ -0,0 +1,48 @@
+import inquirer from "inquirer";
+class Student {
+    name;
+    constructor(n) {
+        this.name = n;
+    }
+}
+class person {
+    student = [];
+    addstudent(obj) {
+        this.student.push(obj);
+    }
+}
+const persons = new person();
+const programstart = async (persons) => {
+    do {
+        console.log("Welcome guest");
+        const ans = await inquirer.prompt({
+            type: "list",
+            message: "who do you want to talk to?",
+            name: "select",
+            choices: ["self", "student"]
+        });
+        if (ans.select == "self") {
+            console.log("im talking to my self");
+            console.log("i'm feeling well");
+        }
+        if (ans.select == "student") {
+            const ans = await inquirer.prompt({
+                type: "input",
+                message: "whom do you want to talk to?",
+                name: "student",
+            });
+            const student = persons.student.find(val => val.name == ans.student);
+            if (!student) {
+                const name = new Student(ans.student);
+                persons.addstudent(name);
+                console.log(`hello im ${name.name}, and im fine `);
+                console.log(persons.student);
+            }
+            if (student) {
+                console.log(`hello im ${student.name}, and im fine.................. `);
+                console.log(persons.student);
+            }
+        }
+    } while (true);
+};
+programstart(persons);
